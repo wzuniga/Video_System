55c7ed7db6f9107067235d995049866f3b1fc5e6
Walter Zuniga <wzuniga@episunsa.edu.pe>
2016-09-20 19:24:34 -0500

    Adding gitignore

---
+++ public/javascripts/main.js
@@ -0,0 +1,33 @@
+var socket = io.connect('http://localhost:3000', { 'forceNew': true });
+
+socket.on('messages', function(data) {
+  console.log(data);
+  render(data);
+})
+
+function render (data) {
+  var html = data.map(function(elem, index) {
+    return(`<div class="well well-sm">
+      <strong>${elem.author}</strong>:
+      <em>${elem.text}</em>
+      </div>`);
+    }).join(" ");
+
+    document.getElementById('messages').innerHTML = html;
+  }
+
+  function addMessage(e) {
+    var atr = document.getElementById('username').value;
+    var txt = document.getElementById('texto').value;
+
+    if (atr == '' || txt == '' ) return false;
+
+    var message = {
+      author: atr,
+      text: txt
+    };
+    document.getElementById("username").value="";
+    document.getElementById("texto").value="";
+    socket.emit('new-message', message);
+    return false;
+  }
