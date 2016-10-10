f9bf84e20b395425bd2f6f1dfa2b55c6dd0af109
HernanFaustino <chaccachuctaya@gmail.com>
2016-10-10 02:19:01 -0500

    Another test to solve an error.

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,25 +1,18 @@
-var mongoose = require( 'mongoose' );
-var gracefulShutdown;
-
-var dbURI = 'mongodb://localhost/Loc8r';
-mongoose.connect(dbURI);
+var mongoose =  require('mongoose');
+var dbURI = 'mongodb://localhost/video_system';
 
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
 }
+mongoose.connect(dbURI);
 
-/*
-* Mongoose connection Events
-*/
-mongoose.connection.on('connected', function(){
+mongoose.connection.on('connected', function () {
   console.log('Mongoose connected to ' + dbURI);
 });
-
-mongoose.connection.on('error', function(err){
-  console.log('Mongoose connection error: ' + error);
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
 });
-
-mongoose.connection.on('disconnected', function(){
+mongoose.connection.on('disconnected', function () {
   console.log('Mongoose disconnected');
 });
 
@@ -30,31 +23,24 @@ var gracefulShutdown = function (msg, callback) {
   });
 };
 
-/*
-* For nodemon restarts
-*/
+//For nodemon restarts
 process.once('SIGUSR2', function () {
   gracefulShutdown('nodemon restart', function () {
     process.kill(process.pid, 'SIGUSR2');
   });
 });
 
-/*
-* For app termination
-*/
-process.on('SIGINT', function() {
+//For app restarts
+process.on('SIGINT', function () {
   gracefulShutdown('app termination', function () {
     process.exit(0);
   });
 });
 
-/*
-* For Heroku app termination
-*/
+//For Heroku restarts
 process.on('SIGTERM', function() {
   gracefulShutdown('Heroku app shutdown', function () {
     process.exit(0);
   });
 });
-
 require('./videos');
