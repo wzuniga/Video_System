d0cc430a7676a17440da8e3b7d0160943e479c2c
HernanFaustino <chaccachuctaya@gmail.com>
2016-10-30 15:34:53 -0500

    API.

---
+++ app_api/controllers/users.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var Us = mongoose.model('User');
+
+var sendJSONresponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
+module.exports.userList = function (req, res) {
+  Us
+    .find()
+    .exec(function(err, users){
+      if(!users){
+        sendJSONresponse(res, 404, {"message": "No users found."});
+        return;
+      }else if(err){
+        sendJSONresponse(res, 404, err);
+        return;
+      }
+      sendJSONresponse(res, 200, users);
+    });
+};
+
+module.exports.userById = function(req, res){
+  if(req.params && req.params.userid){
+    Us
+    .findById(req.params.userid)
+    .exec(function(err, user){
+      if(!user){
+        sendJSONresponse(res, 404, {"message": "No user found."});
+        return;
+      }else if(err){
+        sendJSONresponse(res, 404, err);
+        return;
+      }
+      sendJSONresponse(res, 200, user);
+    });
+  }else{
+    sendJSONresponse(res, 404, {
+      "message" : "No user in request"
+    });
+  }
+};
