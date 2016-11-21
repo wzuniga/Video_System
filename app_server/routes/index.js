902df3a47da5f90643627ac37259c0e8b9d7955a
Jeyson <jeysong.gaona@gmail.com>
2016-11-21 16:43:40 -0500

    actualizacion presentacion

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -7,5 +7,7 @@ router.get('/', ctrlHome.index);
 
 router.get('/emitir', ctrlHome.emitir);
 router.get('/visualizar', ctrlHome.visualizar);
+router.get('/login', ctrlHome.login);
+router.get('/register', ctrlHome.register);
 
 module.exports = router;
