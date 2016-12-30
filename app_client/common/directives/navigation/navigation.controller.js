f6ac6eb9db0ed4bb8c7ab137b3a55739c364e7fa
HernanFaustino <chaccachuctaya@gmail.com>
2016-12-29 21:18:42 -0500

    Navigation to a directive.

---
+++ app_client/common/directives/navigation/navigation.controller.js
@@ -0,0 +1,37 @@
+(function () {
+
+  angular
+    .module('videx')
+    .controller('navigationCtrl', navigationCtrl);
+
+  navigationCtrl.$inject = ['$location', '$uibModal'];
+  function navigationCtrl($location, $uibModal) {
+    var vm = this;
+    /*
+    vm.currentPath = $location.path();
+
+    vm.isLoggedIn = authentication.isLoggedIn();
+
+    vm.currentUser = authentication.currentUser();
+
+    vm.logout = function() {
+      authentication.logout();
+      $location.path('/');
+    };
+    */
+    vm.popupLoginForm = function(){
+         var modalInstance = $uibModal.open({
+            templateUrl: '/modals/login_modal/login.modal.html',
+            controller: 'loginController as vm'
+         });
+      };
+
+    vm.popupRegisterForm = function(){
+       var modalInstance = $uibModal.open({
+          templateUrl: '/modals/register_modal/register.modal.html',
+          controller: 'registerController as vm'
+       });
+    }; 
+
+  }
+})();
