e498f4b5786b39108b2361ffbaf4d162ba009cbc
johanscc.1912@gmail.com <johanscc.1912@gmail.com>
2016-12-29 21:26:22 -0500

    register y login con autenticacion

--- app_client/modals/login_modal/login.controller.js
+++ app_client/modals/login_modal/login.controller.js
@@ -3,8 +3,18 @@
       .module('videx')
       .controller('loginController', loginController);
    
-   function loginController($uibModalInstance, videxData){
+   loginCtroller.$inject = ['$location','authentication'];
+   function loginController($uibModalInstance, authentication){
       var vm = this;
+
+      vm.pageHeader = {
+         title: 'login'
+      };
+
+      vm.credentials = {
+         email : "",
+         password : ""
+      };
       vm.modal = {
          cancel: function(){
             $uibModalInstance.dismiss('cancel');
@@ -12,6 +22,33 @@
          close: function(result){
             $uibModalInstance.close(result);
          }
+      };
+
+      vm.onSubmit = function () {
+         vm.formError = "";
+            if (!vm.credentials.email || !vm.credentials.password) {
+         vm.formError = "All fields required, please try again";
+            return false;
+         } else {
+         vm.doLogin();
+         }
+      };
+
+
+      vm.doLogin = function() {
+         vm.formError = "";
+         authentication
+            .login(vm.credentials)
+            .success(function(data){
+               vm.modal.close(data);
+            })
+            .error(function(err){
+               vm.formError = err;
+            })
+        .then(function(){
+            $location.search('page', null); 
+            $location.path(vm.returnPage);
+        });
       }; 
    }
 })();
